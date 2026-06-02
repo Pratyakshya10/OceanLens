db70cdda9f76b315764b5b970295a2c16ef2790b
Pratyakshya Mishra <pratyakshya.mishra43@gmail.com>
2026-06-02 13:26:41 +0530

    Connecting paths

--- FRONTEND/frontend/src/App.jsx
+++ FRONTEND/frontend/src/App.jsx
@@ -1,19 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
 import SubmitPage from './pages/SubmitPage'
+import DashboardPage from './pages/DashboardPage'
 import MapPage from './pages/MapPage'
 import StoryPage from './pages/StoryPage'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-slate-950 text-white">
+      <div className="min-h-screen flex flex-col bg-white">
         <Navbar />
-        <Routes>
-          <Route path="/"      element={<SubmitPage />} />
-          <Route path="/map"   element={<MapPage />} />
-          <Route path="/story" element={<StoryPage />} />
-        </Routes>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/"          element={<HomePage />} />
+            <Route path="/submit"    element={<SubmitPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/map"       element={<MapPage />} />
+            <Route path="/story"     element={<StoryPage />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </BrowserRouter>
   )
